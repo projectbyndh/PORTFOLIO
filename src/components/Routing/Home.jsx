6c42fd3a98b3e530312a41511d32b70d0ffb414b
Bibek1604 <npnbibek94@gmail.com>
2025-03-24 08:16:43 +0545

    refactoring

--- src/components/Routing/Home.jsx
+++ src/components/Routing/Home.jsx
@@ -1,13 +1,15 @@
 import First from "../First";
 import Testimonails from "../Testimonails";
-import Services from "./Services";
+import Services from "../Services";
 import React from "react";
+import Index from '../Routing/Index'
 function Home() {
   return (
     <>
     <First />
     <Testimonails />
     <Services />
+    <Index />
     </>
   )
 }
