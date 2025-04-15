ecfff50e0f412653b632e1a7e6cc152bfb52c372
Bibek1604 <npbibek94@gmail.com>
2025-04-15 21:55:17 +0545

    updated version

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Routing/Home';
-import AboutUs from './components/Aboutus';
-import CaseStudies from './components/Case';
-import Contact from './components/Contact';
-import EServices from '../src/components/EServices'
-import Carrers from '../src/components/Carrers'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Routing/Home";
+import AboutUs from "./components/Aboutus";
+import CaseStudies from "./components/Case";
+import Contact from "./components/Contact";
+import EServices from "../src/components/EServices";
+import Carrers from "../src/components/Carrers";
 function App() {
   return (
-    <Router>
-      <div className="App">
-
-
+    <>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/casestudy" element={<CaseStudies />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/carrers" element={<Carrers />} />
-          <Route path='/eservices' element={<EServices/>} />
-
-
+          <Route path="/eservices" element={<EServices />} />
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </>
   );
 }
 
